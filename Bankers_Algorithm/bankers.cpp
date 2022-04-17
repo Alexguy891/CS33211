9aacdfb71c9c4fc42756d803b0b9bab4b6f22926
Alex Ely <ely.m.alexander@gmail.com>
2022-04-17 16:33:08 -0400

    Fixed most of file input

--- Bankers_Algorithm/bankers.cpp
+++ Bankers_Algorithm/bankers.cpp
@@ -3,69 +3,144 @@
 #include <string>
 #include <vector>
 
-void getData(std::vector< std::vector<int> > all, std::vector< std::vector<int> > max, std::vector<int> ava, int &pro, int &res) {
+void getData(std::vector< std::vector<int> > &all, std::vector< std::vector<int> > &max, std::vector<int> &ava, int &pro, int &res) {
+
+    std::cerr << "In getData()..." << '\n';
+
     std::ifstream in;
     in.open("input.csv");
+
+    if(!in) {
+        std::cout << "File failed to open..." << '\n';
+    }
+
+    std::cerr << "File opened" << '\n';
 
     char c;
 
     int i = 0;
-    int j = 0;
-    int k = 0;
     
+    std::vector<int> row;
+
+    in.get(c);
+    //in.get(c);
+    //in.get(c);
     //Need to dynamically push items into rows and push new rows onto vectors (04/14/2022)
-    while(!in.eof()) {
-        while(in.get(c)) {
-            if(c == ' ')
-                k++;
-            else if(c == '\n' || c == '\t' || c == '\r') {
-                j++;
+        while(!in.eof()) {
+            in.get(c);
+            std::cerr << "Looped in.get(c)..." << "\n\n";
+            std::cerr << "c = " << c << '\n';
+            if(c == '\n' || c == '\t' || c == '\r') {
+                std::cerr << "In if(c == '\n' || c == '\t' || c == '\r')..." << '\n';
+
                 i = 0;
-                k = 0;
+                row.clear();
+
+                std::cerr << "i = " << i << '\n';
+                std::cerr << "row cleared" << '\n';                
             }
             else if(c == ',') {
+                std::cerr << "In if(c == ',')..." << '\n';
+
+                if(i == 0) {
+                    std::cerr << "In if(i == 0)..." << '\n';
+
+                    all.push_back(row);
+
+                    std::cerr << "Called all.push_back(row)..." << '\n';
+                }
+                else if(i == 1) {
+
+                    std::cerr << "In if(i == 1)..." << '\n';
+
+                    max.push_back(row);
+
+                    std::cerr << "Called max.push_back(row)..." << '\n';
+                }
+
                 i++;
-                k = 0;
+
+                std::cerr << "i = " << i << '\n';
+
             }
-            else if(i == 0) {
+            else if(i != 2 && c != ' ') {
+
+                std::cerr << "In if(i != 2)..." << '\n';
+
                 int ch = c - '0';
-                all[j][k] = ch;
+
+                std::cerr << "ch = " << ch << '\n';
+
+                row.push_back(ch);
+
+                std::cerr << "Called row.push_back(ch)..." << '\n';
+
+                //max[j][k] = ch;
             }
-            else if(i == 1) {
+            else if(i == 2 && c != ' ') {
+
+                std::cerr << "In if(i == 2)..." << '\n';
+
                 int ch = c - '0';
-                max[j][k] = ch;
-            }
-            else if(i == 2) {
-                int ch = c - '0';
-                ava[k] = ch;
+
+                std::cerr << "ch = " << ch << '\n';
+
+                ava.push_back(ch);
+
+                std::cerr << "Called ava.push_back(ch)" << '\n';
             }
         }
-    }
-    pro = j;
+    pro = all.size();
+
+    std::cerr << "pro = " << pro << '\n';
+
     res = ava.size();
+
+    std::cerr << "res = " << res << '\n';
 }
 
 int main() {
     int pro, res;
 
-    pro = 5;
-    res = 3;
-
     std::vector< std::vector<int> > all;
     std::vector< std::vector<int> > max;
     std::vector<int> ava;
+    
+    getData(all, max, ava, pro, res);
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 3; j++) {
+            std::cout << all[i][j] << " ";
+        }
+        std::cout << '\n';
+    }
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 3; j++) {
+            std::cout << max[i][j] << " ";
+        }
+        std::cout << '\n';
+    }
+
+    for(int i = 0; i < 5; i++) {
+       std::cout << ava[i] << " ";
+    }
+    std::cout << '\n';
 
     int fin[pro], end[pro];
+
     int nee[pro][res];
 
     int step = 0;
 
     for(int i = 0; i < pro; i++)
         fin[i] = 0;
+
     
-    for(int j = 0; j < pro; j++) {
-        for(int k = 0; k < res; k++)
-            nee[j][k] = max[j][k] - all[j][k];
+    for(int i = 0; i < pro; i++) {
+        for(int j = 0; j < res; j++) {
+            nee[i][j] = max[i][j] - all[i][j];
+        }
     }
 
     for(int i = 0; i < pro; i++) {
